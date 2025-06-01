3f7eb4976ebc5d4c104f86f6915622d085b2d07f
bigyanse <contact.bigyanse@gmail.com>
2025-06-01 18:32:33 +0545

    `refactor`: using `toml` based placeholders

--- {{ petor.project.slug }}/src/config/urls.py
+++ {{ petor.project.slug }}/src/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for {{ petor.project_name }} project.
+URL configuration for {{ petor.project.name }} project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
--- {{ petor.project.slug }}/src/config/wsgi.py
+++ {{ petor.project.slug }}/src/config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for {{ petor.project_name }} project.
+WSGI config for {{ petor.project.name }} project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
